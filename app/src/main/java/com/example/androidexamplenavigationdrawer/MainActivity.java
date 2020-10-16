5dd312ad655588aa03f5f0ea5d750e25ab369928
Raquel <raquel.maria@vr.com.br>
2020-10-16 11:47:03 -0300

    Editando fragment navigation

--- app/src/main/java/com/example/androidexamplenavigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/androidexamplenavigationdrawer/MainActivity.java
@@ -39,7 +39,7 @@ public class    MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
-    
+
 
     @Override
     public boolean onSupportNavigateUp() {
